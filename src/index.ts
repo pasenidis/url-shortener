3e0a6a87cbbed3ef9acc44316bdae0ff8e738024
Edward Pasenidis <pasenidis@localhost.localdomain>
2020-08-02 02:01:25 +0300

    feat: create main endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Typescript is good.');
+import express, { Request, Response, Application } from 'express';
+import api from './api';
+import figlet from 'figlet';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 1337;
+const print = (text: string) => figlet(text, (err, data) => err ? console.error(err) : console.log(data));
+
+app.use('/api', api);
+app.listen(port, () => {
+    print('URL Shortener')
+    console.log('Created by Edward Pasenidis');
+});
